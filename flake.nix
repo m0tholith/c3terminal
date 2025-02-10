e2745f451c52aaef5a82d93e726110dcbc2a9076
m0tholith <48518572+m0tholith@users.noreply.github.com>
2025-02-10 20:46:57 +0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    c3c = {
+      flake = true;
+      url = "github:c3lang/c3c";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      c3c,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          (c3c.outputs.packages.${system}.default)
+          glfw
+        ];
+      };
+    };
+}
